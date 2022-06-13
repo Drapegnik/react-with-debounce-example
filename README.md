514edcd0632b902032a388c18f1a1abe4b008586
Ivan Pazhitnykh <Drapegnik@users.noreply.github.com>
2022-06-13 15:18:37 +0300

    update readme

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # react-with-debounce-example
-Created with CodeSandbox
+
+## tasks
+
+1. Input, which displays value under it (in some element) after certain time (f.e. `1s`), use debounce
+
+2. After debounced value appears, after certain time (e.g. `5s`), both values are cleared
+
+3. Display countdown on the right of the input, which updates every second and shows how much time remaining before clearing
--- src/index.js
+++ src/index.js
@@ -2,19 +2,6 @@ import React, { useState, useMemo, useRef } from "react";
 import ReactDOM from "react-dom";
 import debounce from "lodash.debounce";
 
-// const fn = debounce(callback, timeout);
-// https://lodash.com/docs/4.17.11#debounce
-
-// 1. Input, which displays value under it (in some element)
-// after certain time (f.e. 1s), use debounce
-
-// 2. After debounced value appears,
-// after certain time (e.g. 5s), both values are cleared
-
-// 3. Display countdown on the right of the input,
-// which updates every second and shows how much time
-// remaining before clearing
-
 import "./styles.css";
 
 const SECOND = 1000;
